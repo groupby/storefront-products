6aa7d0ccb90b0afffc11b4f6941a9db41dcf631a
Ben Teichman <ben.l.teichman@gmail.com>
2017-08-31 13:07:14 -0400

    working variant selector

--- src/swatch-product/index.ts
+++ src/swatch-product/index.ts
@@ -1,3 +1,25 @@
-import { view } from '@storefront/core';
+import { alias, tag, utils, Tag } from '@storefront/core';
 
-view('gb-swatch-product', require('./index.html'));
+@alias('swatchProduct')
+@tag('gb-swatch-product', require('./index.html'))
+class SwatchProduct {
+
+  state: any = {
+    options: () => {
+      return this.props.product.variants.map((variant) => utils.dot.get(variant, this.props.field));
+    }
+  };
+}
+
+interface SwatchProduct extends Tag { }
+namespace SwatchProduct {
+  export interface Props {
+    field: string;
+    product: {
+      data: any;
+      variants: any[];
+    };
+  }
+}
+
+export default SwatchProduct;
