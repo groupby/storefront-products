1298d39cbfa602fd78ea989596f6176549f7463e
Ben Teichman <ben.l.teichman@gmail.com>
2017-08-31 11:12:24 -0400

    initial

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Product from './product';
 import Products from './products';
 import './simple-card';
+import './swatch-product';
+import SwatchVariantControls from './swatch-variant-controls';
 import './tile';
+import Variants from './variants';
 
-export { Product, Products };
+export { Product, Products, SwatchVariantControls, Variants };
