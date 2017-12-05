4866160fca5e2998ff2a737f4adb66984ee6b21f
Bruce Chen <bruce.chen@groupbyinc.com>
2017-12-05 16:00:38 -0500

    Add past-purchases-products here to reuse products

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import PastPurchasesProducts from './past-purchases-products';
 import Product from './product';
 import Products from './products';
 import './simple-card';
 import SwatchVariantControls from './swatch-variant-controls';
 import './tile';
 
-export { Product, Products, SwatchVariantControls };
+export { Product, Products, PastPurchasesProducts, SwatchVariantControls };
