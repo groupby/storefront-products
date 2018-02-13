172b52b692307a274763aa90f77e07659b8faec1
mengqi <treeforever7@gmail.com>
2018-02-13 17:17:36 -0500

    rebase

--- src/products/index.ts
+++ src/products/index.ts
@@ -25,16 +25,37 @@ class Products {
   };
 
   init() {
-    this.flux.on(Events.PRODUCTS_UPDATED, this.updateProducts);
-    // this.fetchTrendingProducts();
+    // we force an update on init to avoid race condition issues
+    switch (this.props.storeSection) {
+      case StoreSections.PAST_PURCHASES:
+        this.updatePastPurchaseProducts(this.select(Selectors.pastPurchaseProducts));
+        this.flux.on(Events.PAST_PURCHASE_PRODUCTS_UPDATED, this.updatePastPurchaseProducts);
+        break;
+      case StoreSections.SEARCH:
+        this.updateProducts();
+        this.flux.on(Events.PRODUCTS_UPDATED, this.updateProducts);
+        break;
+    }
+
+    this.fetchTrendingProducts();
   }
 
+  productTransformer: Transformer = ({ data, meta }: Product) =>
+    ({ ...ProductTransformer.transformer(this.structure)(data), meta })
+
   updateProducts = () => {
+    const products = this.select(Selectors.productsWithPastPurchase, this.config.recommendations.idField);
     this.set({
-      products: this.select(Selectors.productsWithPastPurchase, this.config.recommendations.idField)
-        .map(this.productTransformer)
+      products: products.map(this.productTransformer)
     });
   }
+
+  updatePastPurchaseProducts = (products: any = []) =>
+    this.set({
+      products: products
+        .map(({ meta, ...data }) => ({ data, meta }))
+        .map(this.productTransformer)
+    })
 
   fetchTrendingProducts = () => {
     const request = {
@@ -50,7 +71,6 @@ class Products {
     // tslint:disable-next-line:max-line-length
     const url = `https://${this.config.customerId}.groupbycloud.com/wisdom/v2/public/recommendations/products/_getTrending`;
     this.flux.store.dispatch(this.flux.actions.customFetchProducts({ request, url }));
-
   }
 }
 
