13681d2cfa3e5b10b604e337b4e8a9a13e183ce4
Johann Tutor <johann.tutor@groupbyinc.com>
2018-10-05 10:04:59 -0400

    Use prototype aliasing

--- src/swatch-variant-controls/index.ts
+++ src/swatch-variant-controls/index.ts
@@ -4,14 +4,14 @@ import Product from '../product';
 @consume('product')
 @tag('gb-swatch-variant-controls', require('./index.html'))
 class SwatchVariantControls {
-  $product: Product.State;
+  aliases: SwatchVariantControls.Aliases;
 
-  options = () => this.$product.variants.map((variant) => utils.dot.get(variant, this.props.field));
+  options = () => this.aliases.product.variants.map((variant) => utils.dot.get(variant, this.props.field));
 
-  onClick = (index: number) => this.$product.onSelect && this.$product.onSelect(index, true);
+  onClick = (index: number) => this.aliases.product.onSelect && this.aliases.product.onSelect(index, true);
 
   onSelect = (index: number, isActive: boolean) =>
-    this.$product.onSelect && this.$product.onSelect(isActive ? index : -1);
+    this.aliases.product.onSelect && this.aliases.product.onSelect(isActive ? index : -1);
 }
 
 interface SwatchVariantControls extends Tag<SwatchVariantControls.Props> {}
@@ -19,6 +19,10 @@ namespace SwatchVariantControls {
   export interface Props {
     field: string;
     onSelect(index: number, persist?: boolean);
+  }
+
+  export interface Aliases {
+    product: Product.State;
   }
 }
 
